65675ce8aa123064c68d5d84587be6a69e629a3c
ivan-carballo <ivanrallyasfalto@hotmail.com>
2024-06-28 11:07:36 +0200

    model para estadisticas de Data

---
+++ back/models/Model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userSchema  = new mongoose.Schema({
+
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    role: {
+        type: String,
+        enum: ['user', 'admin', 'superadmin'],
+        default: 'user'
+    }
+
+
+})
+
+
+const userModel = mongoose.model("users",userSchema);
+
+export default userModel;
