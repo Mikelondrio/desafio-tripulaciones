2bcc71782477b5287df78d4c6be4c04ce9298b36
ivan-carballo <ivanrallyasfalto@hotmail.com>
2024-06-28 09:43:46 +0200

    Model, controllers y router de user

---
+++ back/routes/userRouter.js
@@ -0,0 +1,18 @@
+import {Router} from "express";
+
+import userAPIController from "../controllers/apiControllers/userAPIController.js";
+
+
+const router  = Router();
+
+
+router.post("/",userAPIController.create);
+router.post("/find",userAPIController.getByProperty);
+router.post("/remove/:id",userAPIController.remove);
+router.post("/update/:id",userAPIController.update);
+
+
+router.get("/",userAPIController.getAll);
+
+
+export default router;
