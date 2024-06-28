c84215771e5f8d41b13a56afcc2e2741c6d52814
Mikelondrio <mikelon_santxez@hotmail.com>
2024-06-28 12:03:23 +0200

    register y login en back

--- back/controllers/controllers/userController.js
+++ back/controllers/controllers/userController.js
@@ -11,6 +11,17 @@ const getAll = async(userId=null)=> {
     }
 }
 
+function getUserData(user){
+    return {
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        password: user.password,
+        role: user.role
+        
+    }
+}
+
 const getById = async(id) =>{
     try {
             const datos = await userModel.findById(id);
@@ -29,6 +40,64 @@ const getByProperty = async(property,value) =>{
     } catch (error) {
         return null;
     }
+}
+const login = async(data) =>{
+    console.log("datalogin",data)
+    const {email,username,password} = data;
+    if((!email && !username ) || !password){
+        return {error:"faltan datos",status:400};
+    }
+    try {
+        let user;
+        if(email){
+            const users = await getByProperty("email",email,true);
+            user = users[0];
+        }
+        else{
+            const users = await getByProperty("username",username,true);
+            user = users[0];
+        }
+        console.log("usurio",user);
+        if(!user){
+            return {error:"No existe el usurio",status:400};
+        }
+        console.log("contraseña",password,user.password);
+        const isPasswordCorrect = await bcrypt.compare(password,user.password);
+        if(!isPasswordCorrect){
+            return {error:"Combinación de usuario y contraseña erroneos",status:400};
+        }
+        console.log("login user",user)
+        const token = jwt.sign({_id:user._id,username:user.username,role:user.role},process.env.JWT_SECRET,{expiresIn: 60 * 60 * 24})
+        const userData ={
+            _id: user._id,
+            username: user.username,
+            role: user.role,
+        }
+        return {token,user:userData};
+
+        
+    } catch (error) {
+        console.error(error);
+        return {error:"Ha habido un error",status:500};
+    }
+}
+const register = async(data) => {
+    console.log("dataregister",data)
+    const {email,username,password,passwordRepeat} = data;
+    if(!email || !username || !password || !passwordRepeat){
+        return {error:"Falta alguno de los campos"};
+    }
+    if(password !== passwordRepeat){
+        return {error:"Las contraseñas no coinciden"};
+    }
+    const userData = {
+        email,
+        username,
+        password,
+        role:"user"
+    }
+    const user = await create(userData);
+    return user;
 }
 
 const create = async(data) =>{
@@ -65,8 +134,11 @@ const remove = async(id) =>{
 
 export const functions = {
     getAll,
+    getUserData,
     getById,
     getByProperty,
+    login,
+    register,
     create,
     update,
     remove
