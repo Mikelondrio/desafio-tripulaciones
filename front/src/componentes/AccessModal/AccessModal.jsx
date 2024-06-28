06788ef058fe9e0cb6bf89f724e0aff7a32b7531
Gandino1984 <andinogerman@gmail.com>
2024-06-28 10:04:38 +0200

    AccessModal create

--- front/src/componentes/AccessModal/AccessModal.jsx
+++ front/src/componentes/AccessModal/AccessModal.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
+import { useRef } from 'react'
 
-function AccessModal() {
-  return (
-    <div>
-      
-    </div>
-  )
+
+function AccessModal(){
+
+    const accessModalRef= useRef(null);
+
+    function toggleAccessModal() {
+        if(!accessModalRef.current) {
+          return;
+        }
+        accessModalRef.current.hasAttribute('open') ? accessModalRef.current.close() : accessModalRef.current.showModal();
+      }
+
+    return (
+      <>
+
+        <button onClick={toggleAccessModal}>Modal</button>
+
+          <div className='accessModalContainer'>
+            
+            <dialog ref={accessModalRef}>
+                <button><ion-icon name="close"></ion-icon></button>
+                <p>modal content</p>
+            </dialog>
+          
+          </div>
+      </>
+    )
+    
 }
 
 export default AccessModal
