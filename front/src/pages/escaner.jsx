3381269436cb910a1c2d1e3503656a4cf598ce05
ivan-carballo <ivanrallyasfalto@hotmail.com>
2024-07-01 09:48:32 +0200

    Funcion para enviar HTML hacia Data

--- front/src/pages/escaner.jsx
+++ front/src/pages/escaner.jsx
@@ -1,13 +1,42 @@
 import { useState, useEffect } from "react";
 import { Navbar } from "../componentes/navbar.jsx";
-import Cookies from 'js-cookie';
-import { sha256 } from 'js-sha256';
 
 
 
 
 
 function Escaner() {
+
+
+
+
+    async function webSendAPI(data) {
+        fetch('', data)
+        .then(data => {
+            if (!data.ok) {
+              throw Error(data.status);
+             }
+             return data.json();
+            }).then(update => {
+            console.log(update);
+            }).catch(e => {
+            console.log(e);
+            });
+      }
+
+      async function buttonWebSend(e) {
+        const webArraySend = {'': ''}
+
+        const data = {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(webArraySend),
+            };
+
+            const webSend = await webSendAPI(data)
+    }
 
 
 
