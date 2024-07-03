4e924e09308bc0f8ae1f77b06a3e11141b192755
Gandino1984 <andinogerman@gmail.com>
2024-07-03 13:23:06 +0200

    animation navbar

--- front/src/componentes/Header/Navbar.jsx
+++ front/src/componentes/Header/Navbar.jsx
@@ -10,12 +10,10 @@ export const Navbar = () => {
         <>
             <nav>
                 <div className={styles.container}>
-                 
                     <div className={styles.navbar}>
                         <NavLink to="/inicio" className={styles.link}>Inicio</NavLink>
                         <NavLink to="/resultados" className={styles.link}>Resultados</NavLink>
                         <NavLink to="/contactar" className={styles.link}>Contactar</NavLink>
-                        
                         <button type="button" className="menu-button-text">Entrar</button>
                     </div>
                 </div>
