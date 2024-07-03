570f53119d298a3e5c25a07707f0b3cdbe9bc1e4
Gandino1984 <andinogerman@gmail.com>
2024-07-03 12:04:34 +0200

    fix header

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -10,9 +10,6 @@ function App() {
   return (
     <>
         <RouterProvider router={router} />
-        <Header />
-        <Footer />
-
     </>
   )
 }
--- front/src/router.jsx
+++ front/src/router.jsx
@@ -1,32 +1,32 @@
 import { createBrowserRouter } from "react-router-dom";
-import  Inicio  from "./pages/Inicio.jsx";
-import  Contactar  from "./pages/Contactar.jsx";
-import  Resultados  from "./pages/Resultados.jsx";
-
+import Layout from './componentes/Layout.jsx'
+import Inicio from "./pages/Inicio.jsx";
+import Contactar from "./pages/Contactar.jsx";
+import Resultados from "./pages/Resultados.jsx";
 
 const router = createBrowserRouter([
-
-        {
-          path: "/",
-          element: <Inicio />
-        },
-  
-        {
-          path: "/inicio",
-          element: <Inicio />
-        },
-
-        {
-          path: "/resultados",
-          element: <Resultados />
-        },
-
-        {
-          path: "/contactar",
-          element: <Contactar />
-        }
- 
-  ]);
-
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Inicio />,
+      },
+      {
+        path: "/inicio",
+        element: <Inicio />,
+      },
+      {
+        path: "/resultados",
+        element: <Resultados />,
+      },
+      {
+        path: "/contactar",
+        element: <Contactar />,
+      },
+    ],
+  },
+]);
 
 export default router;
