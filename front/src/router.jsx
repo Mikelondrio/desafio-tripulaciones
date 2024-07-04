18b2b0410a7cadf38753c0ff031095144069bbd6
ignaciochagar <43643985+ignaciochagar@users.noreply.github.com>
2024-07-04 10:13:44 +0200

    cambio a register

--- front/src/router.jsx
+++ front/src/router.jsx
@@ -25,6 +25,10 @@ const router = createBrowserRouter([
         path: "/contactar",
         element: <Contactar />,
       },
+      {
+        path: "/register",
+        element: <Contactar />,
+      },
     ],
   },
 ]);
