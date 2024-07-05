41473b5fa70f9d531669f7f01ceabf325131cf96
ivan-carballo <ivanrallyasfalto@hotmail.com>
2024-07-05 14:07:00 +0200

    Creado ternario de navbar y pagina vacia para panel de user/admin

--- front/src/componentes/Header/Navbar.jsx
+++ front/src/componentes/Header/Navbar.jsx
@@ -1,13 +1,23 @@
 import { NavLink } from "react-router-dom";
 import styles from './Navbar.module.css';
+import { useState } from "react";
 
 export const Navbar = ({ className }) => {
+  const [recarga, setRecarga] = useState(false)
+
+    setTimeout(() => {
+      setRecarga(true)
+    }, 1000);
+
+  const token = localStorage.getItem('token')
+
   return (
     <nav className={className}>
       <div className={styles.container}>
         <div className={styles.navbar}>
           <NavLink to="/inicio" className={styles.link}>Inicio</NavLink>
           <NavLink to="/resultados" className={styles.link}>Resultados</NavLink>
+          {token ? <NavLink to="/user" className={styles.link}>Perfil</NavLink> : <></>}
           <NavLink to="/contactar" className={styles.link}>Contactar</NavLink>
           <NavLink to="/registro" className={styles.entrarBtn}>Entrar</NavLink>
         </div>
