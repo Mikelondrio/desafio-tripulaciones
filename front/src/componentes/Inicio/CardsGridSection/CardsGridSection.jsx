1500d76457f24c2b2fc38fd5a514f20461ceeddd
ignaciochagar <43643985+ignaciochagar@users.noreply.github.com>
2024-07-07 16:10:31 +0200

    styles home

--- front/src/componentes/Inicio/CardsGridSection/CardsGridSection.jsx
+++ front/src/componentes/Inicio/CardsGridSection/CardsGridSection.jsx
@@ -2,26 +2,33 @@
 import React from 'react';
 import styles from './CardsGridSection.module.css';
 import Senora_con_ordenador from '../../../assets/Home/Senora_con_ordenador.jpg';
-import Madurito_sexy  from '../../../assets/Home/Madurito_sexy.jpg';
+import Madurito_sexy from '../../../assets/Home/Madurito_sexy.jpg';
 import ordenador from '../../../assets/Home/ordenador.jpg';
-
 
 const cardsData = [
     {
-        title: '¿ QUE HACE SILVER SITE ?',
-        description: 'Te permite loren impsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        title: '¿QUÉ HACE SILVER TEST?',
+        description: 'Te permite detectar los posibles fallos que puede tener tu página web en cuanto a accesibilidad.',
         image: Senora_con_ordenador,
-        
+        isReversed: false,
+        backgroundColor: '#3FB58F',
+        textcolor: '#3FB58F'
     },
     {
-        title: '¿ QUE RESULTADOS RECIBIRÁS ?',
-        description: 'This is the second card.',
+        title: '¿QUÉ RESULTADOS RECIBIRÁS?',
+        description: 'Un completo informe con los errores detectados en tu página web y las posibles soluciones para mejorar la accesibilidad.',
         image: ordenador,
+        isReversed: true,
+        backgroundColor: '#F8A800',
+        textcolor: '#F8A800'
     },
     {
-        title: '¿ QUE NECESITAS ?',
-        description: 'This is the third card.',
+        title: '¿QUÉ NECESITAS?',
+        description: 'Simplemente la URL de tu página web. Nosotros nos encargamos del resto para ofrecerte un análisis detallado.',
         image: Madurito_sexy,
+        isReversed: false,
+        backgroundColor: '#3A4683',
+        textcolor: '#3A4683'
     }
 ];
 
@@ -29,11 +36,14 @@ function CardsGridSection() {
     return (
         <div className={styles.cardsGrid}>
             {cardsData.map((card, index) => (
-                <div key={index} className={styles.card}>
-                    <img src={card.image} alt={card.title} className={styles.cardImage} />
+                <div key={index} className={`${styles.card} ${card.isReversed ? styles.reversed : ''}`}>
                     <div className={styles.cardContent}>
-                        <h2 className={styles.cardTitle}>{card.title}</h2>
+                        <h2 style={{color: card.textcolor}} className={styles.cardTitle}>{card.title}</h2>
                         <p className={styles.cardDescription}>{card.description}</p>
+                        <button style={{backgroundColor: card.backgroundColor}} className={styles.cardButton}>Saber más</button>
+                    </div>
+                    <div className={styles.imageContainer}>
+                        <img src={card.image} alt={card.title} className={styles.cardImage} />
                     </div>
                 </div>
             ))}
