a1ce5e8fd830eb9394e7c47ca448d3bf9a40fd7d
Gandino1984 <andinogerman@gmail.com>
2024-07-08 09:31:27 +0200

    gestionUsuarios

--- front/src/componentes/users/UserManagement.jsx
+++ front/src/componentes/users/UserManagement.jsx
@@ -1,16 +1,16 @@
 // UserManagement.js
 import React, { useState } from 'react';
-import HeaderUsermanagement from './headerUserManagement';
+import HeaderUserManagement from './headerUserManagement';
 import UserCard from './UserCard';
 import styles from './UserManagement.module.css';
 
 const initialUsers = [
-  { name: 'Marina Hirst', username: 'marina_h', photo: 'url_to_photo_1' },
-  { name: 'Luis Delgado', username: 'ldelgado', photo: 'url_to_photo_2' },
-  { name: 'Teresa Castro', username: 'teresacastro', photo: 'url_to_photo_3' },
-  { name: 'Carmen Gil', username: 'carmen_gil', photo: 'url_to_photo_4' },
-  { name: 'Ian Jander', username: 'ijander', photo: 'url_to_photo_5' },
-  { name: 'Lucia Llus', username: 'lucia_llus', photo: 'url_to_photo_6' },
+  { name: 'Marina Hirst', username: 'marina_hirst_insta' },
+  { name: 'Luis Delgado', username: 'luis_delgado_insta' },
+  { name: 'Teresa Castro', username: 'teresa_castro_insta' },
+  { name: 'Carmen Gil', username: 'carmen_gil_insta' },
+  { name: 'Ian Jander', username: 'ian_jander_insta' },
+  { name: 'Lucia Llus', username: 'lucia_llus_insta' },
 ];
 
 const UserManagement = () => {
@@ -22,12 +22,12 @@ const UserManagement = () => {
 
   const user = {
     name: 'Antonio Díaz',
-    photo: 'url_to_antonio_photo'
+    photo: 'url_to_antonio_photo' // Actualiza esto con la URL de la foto de Antonio si es necesario
   };
 
   return (
     <div className={styles.userManagement}>
-      <HeaderUsermanagement user={user} />
+      <HeaderUserManagement user={user} />
       <h1>Usuarios</h1>
       <div className={styles.userCardsContainer}>
         {users.map((user, index) => (
