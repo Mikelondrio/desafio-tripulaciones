e5db0ecc782d1fde2f8f194019b48b79438aa31c
ignaciochagar <43643985+ignaciochagar@users.noreply.github.com>
2024-07-09 09:49:07 +0200

    bajo cambios german

--- front/src/router.jsx
+++ front/src/router.jsx
@@ -4,7 +4,7 @@ import Inicio from "./pages/Inicio.jsx";
 import Contactar from "./pages/Contactar.jsx";
 import Resultados from "./pages/Resultados.jsx";
 import Registro from "./pages/Registro.jsx";
-import { GestionUsuarios } from './pages/GestionUsuarios.jsx'
+// import { GestionUsuarios } from './pages/GestionUsuarios.jsx'
 import Perfil from "./pages/perfil.jsx";
 import {ListRegisteredUsers} from "./pages/userlist.jsx"
 import { GraphicHistory } from "./pages/history.jsx"
