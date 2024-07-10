6026918b08988b920d36e5c2fc90eae717cd23b5
ignaciochagar <43643985+ignaciochagar@users.noreply.github.com>
2024-07-11 00:02:58 +0200

    Gif animado

--- front/src/componentes/Preloader.jsx
+++ front/src/componentes/Preloader.jsx
@@ -1,11 +1,13 @@
-// Preloader.jsx
 import React from 'react';
 import styles from './Preloader.module.css'; // Importa los estilos del preloader
 
 const Preloader = () => {
   return (
     <div className={styles.preloaderOverlay}>
-      <img src="/preloader.gif" alt="Loading..." className={styles.preloader} />
+      <div className={styles.preloaderContainer}>
+        <img src="/Logo_animado.gif" alt="Cargando Silver Site" className={styles.preloader} />
+        <p className={styles.loadingText}>Cargando Silver Site</p>
+      </div>
     </div>
   );
 };
