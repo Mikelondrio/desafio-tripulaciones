c4f469bbc1c324fe12a72fe91a34485ce767098c
Mikelondrio <mikelon_santxez@hotmail.com>
2024-07-10 14:23:33 +0200

    subtitulos de categorias

--- front/src/pages/Results.jsx
+++ front/src/pages/Results.jsx
@@ -43,18 +43,22 @@ export function Results() {
             <article id="result-details">
                 <div>
                     <h2>Operable</h2>
+                    <h5>Los componentes de la interfaz de usuario y la navegación deben ser manejables.</h5>
                     <OperableDetail/>
                 </div>
                 <div>
                     <h2>Perceptible</h2>
+                    <h5>La información y los componentes de la interfaz de usuario deben ser mostrados a los usuarios en formas que ellos puedan entender.</h5>
                     <PerceptibleDetail/>
                 </div>
                 <div>
                     <h2>Robusto</h2>
+                    <h5>El contenido deber ser suficientemente robusto para que pueda ser bien interpretado por una gran variedad de agentes de usuario, incluyendo tecnologías de asistencia.</h5>
                     <RobustDetail/>
                 </div>
                 <div>
                     <h2>Entendible</h2>
+                    <h5>La información y las operaciones de usuarios deben ser comprensibles.</h5>
                     <UnderstandableDetail/>
                 </div>
             </article>
