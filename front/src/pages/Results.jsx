35ed2f3bce9a4606ba872e1a514734784ac23dc8
ignaciochagar <43643985+ignaciochagar@users.noreply.github.com>
2024-07-11 10:48:24 +0200

    detalle de analisis desplegable de barras

--- front/src/pages/Results.jsx
+++ front/src/pages/Results.jsx
@@ -5,63 +5,46 @@ import AnalysisPerceptible from "../componentes/Graphics/category/PerceptibleGra
 import AnalysisRobust from "../componentes/Graphics/category/RobustGraphic"
 import AnalysisUnderstandable from "../componentes/Graphics/category/UnderstandableGraphic"
 import './Results.css'
-import DetailOperable from "../componentes/Graphics/details/DetailOperable"
+import DetailSelector from "../componentes/Graphics/details/DetailSelector"
+/* import DetailOperable from "../componentes/Graphics/details/DetailOperable"
 import DetailPerceptible from "../componentes/Graphics/details/DetailPerceptible"
 import RobustDetail from "../componentes/Graphics/details/DetailRobust"
 import DetailUnderstandable from "../componentes/Graphics/details/DetailUnderstandable"
-
+ */
 
 export function Results() {
 
     return (
         <div id='index-cuerpo'>
-            <h2>Resultados</h2>    
-                <div id="analysis-genral">
-                    <AnalysisGeneral/>
-                  
+            <h1>Resultados</h1>
+            <div id="analysis-genral">
+                <AnalysisGeneral />
+
+            </div>
+            <div id="result-canvas">
+                <div className="analysis-item">
+                    <AnalysisOperable />
+
                 </div>
-                <div id="result-canvas">
-                    <div className="analysis-item">
-                        <AnalysisOperable/>
-                       
-                    </div>
-                    <div className="analysis-item">
-                        <AnalysisPerceptible/>
-                     
-                    </div>
-                    <div className="analysis-item">
-                        <AnalysisRobust/>
-                      
-                    </div>
-                    <div className="analysis-item">
-                        <AnalysisUnderstandable/>
-                      
-                    </div>
-                
-        </div>
-            
-            <article id="result-details">
-                <div>
-                    <h2>Operable</h2>
-                    <h5>Los componentes de la interfaz de usuario y la navegación deben ser manejables.</h5>
-                    <DetailOperable/>
+                <div className="analysis-item">
+                    <AnalysisPerceptible />
+
                 </div>
-                <div>
-                    <h2>Perceptible</h2>
-                    <h5>La información y los componentes de la interfaz de usuario deben ser mostrados a los usuarios en formas que ellos puedan entender.</h5>
-                    <DetailPerceptible/>
+                <div className="analysis-item">
+                    <AnalysisRobust />
+
                 </div>
-                <div>
-                    <h2>Robusto</h2>
-                    <h5>El contenido deber ser suficientemente robusto para que pueda ser bien interpretado por una gran variedad de agentes de usuario, incluyendo tecnologías de asistencia.</h5>
-                    <RobustDetail/>
+                <div className="analysis-item">
+                    <AnalysisUnderstandable />
+
                 </div>
-                <div>
-                    <h2>Entendible</h2>
-                    <h5>La información y las operaciones de usuarios deben ser comprensibles.</h5>
-                    <DetailUnderstandable/>
-                </div>
-            </article>
+
+            </div>
+
+            <div>
+                <h1>Detalles de Accesibilidad</h1>
+                <DetailSelector />
+            </div>
 
         </div>
     )
